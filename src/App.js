87cf02642ff067ea9c1385188073a80a9b9f9fad
Manuel <manuelvazquez76@yahoo.com>
2023-09-24 13:05:13 -0700

    creates db.json file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
