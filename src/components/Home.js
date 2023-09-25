7db1e20fb925dfa6823ee8a07fa7ace8fb6571c2
Manuel <manuelvazquez76@yahoo.com>
2023-09-25 02:42:51 -0700

    adds button that redirects to menu page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div className="home">
+      <div className="header-container">
+        <h1>Manuel's Pizza Parlor</h1>
+        <p>PIZZA PERFECTION IN EVERY SLICE</p>
+        <Link to="/menu">
+          <button>ORDER NOW!</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
