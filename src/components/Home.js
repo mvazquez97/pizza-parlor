70da054e14be5ed09f7aca2536d8211cb239b5a7
Manuel <manuelvazquez76@yahoo.com>
2023-09-25 02:51:41 -0700

    adds pizza image to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import HomeImage from "../images/Perfect-Pizza-crust.jpg";
 
 function Home() {
   return (
     <div className="home">
-      <div className="header-container">
+      <div
+        className="header-container"
+        style={{ backgroundImage: `url(${HomeImage})` }}
+      >
         <h1>Manuel's Pizza Parlor</h1>
         <p>PIZZA PERFECTION IN EVERY SLICE</p>
         <Link to="/menu">
