f06add5510a467f246f223a0c7bebe094c950860
Manuel <manuelvazquez76@yahoo.com>
2023-09-25 00:31:27 -0700

    successfully updates Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import PizzaLogo from "../images/FreeVector-Pizza-Logo-Template.jpg";
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
 
 function Navbar() {
   return (
@@ -7,7 +9,12 @@ function Navbar() {
       <div className="left-side">
         <img src={PizzaLogo} alt="logo" />
       </div>
-      <div className="right-side"></div>
+      <div className="right-side">
+        <Link to="/">Home</Link>
+        <Link to="/menu">Menu</Link>
+        <Link to="/about">About Us</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
     </div>
   );
 }
