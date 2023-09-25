4cdba1f75fd02b0906904ec4af012fbf617ed80d
Manuel <manuelvazquez76@yahoo.com>
2023-09-25 01:47:47 -0700

    adds onClick event handler for reorder icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import PizzaLogo from "../images/FreeVector-Pizza-Logo-Template.jpg";
 import { Link } from "react-router-dom";
+import ReorderIcon from "@mui/icons-material/Reorder";
 import "../styles/Navbar.css";
 
 function Navbar() {
+  const [showLinks, setShowLinks] = useState(false);
+
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
   return (
     <div className="navbar">
-      <div className="left-side">
+      <div className="left-side" id={showLinks ? "open" : "close"}>
         <img src={PizzaLogo} alt="logo" />
+        <div className="hidden-links">
+          <Link to="/">Home</Link>
+          <Link to="/menu">Menu</Link>
+          <Link to="/about">About Us</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
       </div>
       <div className="right-side">
         <Link to="/">Home</Link>
         <Link to="/menu">Menu</Link>
         <Link to="/about">About Us</Link>
         <Link to="/contact">Contact</Link>
+        <button onClick={toggleLinks}>
+          <ReorderIcon />
+        </button>
       </div>
     </div>
   );
