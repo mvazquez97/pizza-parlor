056a191e527d260d4c8377f1e00380c19922282a
Manuel <manuelvazquez76@yahoo.com>
2023-09-27 00:12:51 -0700

    adds menu route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
+import Menu from "./components/Menu";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Navbar />
         <Switch>
           <Route exact path="/" component={Home}></Route>
+          <Route exact path="/menu" component={Menu}></Route>
         </Switch>
         <Footer />
       </Router>
--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,16 @@
 import React from "react";
+import multiple from "../images/multiple.jpg";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div className="about">
+      <div
+        className="about-top"
+        style={{ backgroundImage: `url(${multiple})` }}
+      ></div>
+      <div className="about-bottom"></div>
+    </div>
+  );
 }
 
 export default About;
