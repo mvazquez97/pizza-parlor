539f056ab5ef3e8a0b9c3c3e69e66120903eff10
Manuel <manuelvazquez76@yahoo.com>
2023-09-30 13:19:05 -0700

    adds classNames to cards for styling

--- src/components/PizzaCard.js
+++ src/components/PizzaCard.js
@@ -5,9 +5,10 @@ function PizzaCard({ pizza }) {
 
   return (
     <div className="pizza-card">
-      <h2>{pizza.name}</h2>
+      <h2 className="pizza-name">{pizza.name}</h2>
       <img src={pizza.img} alt={pizza.name} />
-      <p>{pizza.price}</p>
+      <p className="pizza-price">{pizza.price}</p>
+      <p className="pizza-toppings"> {pizza.toppings} </p>
     </div>
   );
 }
