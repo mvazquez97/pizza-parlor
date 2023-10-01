e9c42a9d274f967d28bb607b5593e3ffc4936f11
Manuel <manuelvazquez76@yahoo.com>
2023-10-01 00:53:29 -0700

    creates contact form where user enters info and message submission

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,15 +1,79 @@
 import React, { useState } from "react";
 
 function ContactUs() {
-  const [pizza, setPizza] = useState([]);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
-  const addToCart = () => {
-    fetch("http://localhost:3000/pizzas", {
-      method: "POST",
-      headers: {},
-    });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("/api/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        // Successfully submitted the form
+        // Display a success message here
+      } else {
+        // Handle errors or display error messages to the user
+      }
+    } catch (error) {
+      console.error("Error submitting the form:", error);
+    }
   };
-  return <div>ContactUs</div>;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  return (
+    <div>
+      <h2>Contact Us</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default ContactUs;
